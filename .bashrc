5e467ec146979b5948086f971a212642daa4599b
forest reider <forest.reider@belimo.ch>
2025-01-06 11:34:10 +0100

    update config files for various shells and prompt

---
+++ .bashrc
@@ -0,0 +1,63 @@
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+    . /etc/bashrc
+fi
+
+# User specific environment
+if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
+    PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+fi
+export PATH
+
+# Uncomment the following line if you don't like systemctl's auto-paging feature:
+# export SYSTEMD_PAGER=
+
+# User specific aliases and functions
+if [ -d ~/.bashrc.d ]; then
+    for rc in ~/.bashrc.d/*; do
+        if [ -f "$rc" ]; then
+            . "$rc"
+        fi
+    done
+fi
+unset rc
+
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+if [ -f ~/.local/share/blesh/ble.sh ]; then
+    source ~/.local/share/blesh/ble.sh
+fi
+if [ -x "$(command -v vivid)" ]; then
+    export LS_COLORS="$(vivid generate catppuccin-mocha)"
+fi
+if [ -x "$(command -v starship)" ]; then
+    export STARSHIP_CONFIG=~/.config/starship/starship.toml
+    eval "$(starship init bash)"
+fi
+if [ -x "$(command -v micro)" ]; then
+    export EDITOR=$(which micro)
+    export VISUAL=$(which micro)
+    # micro editor setting
+    export  MICRO_TRUECOLOR=1
+    # makes shortxcuts in micro work
+    export TERM=xterm-256color
+fi
+if [ -x "$(command -v eza)" ]; then
+    alias ll='eza -lah'
+fi
+if [ -x "$(command -v flatpak)" ]; then
+    alias zed='flatpak run dev.zed.Zed'
+    alias edge='flatpak run com.microsoft.Edge'
+    alias zen='flatpak run io.github.zen_browser.zen'
+fi
+
+# eval "$(fzf --bash)"
+# export=FZF_DEFAULT_COMMAND 'fd --color=always --hidden --follow --no-ignore --exclude .git "" ~'
+# export FZF_DEFAULT_OPTS="\
+# export FZF_COLOR="--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a"
+# export FZF_OPTS="--tmux 80% --layout=default $FZF_COLOR"
+# export FZF_DEFAULT_OPTS=$FZF_OPTS
+# export FZF_CTRL_R_OPTS=$FZF_OPTS
+# export FZF_CTRL_T_OPTS=$FZF_OPTS
+# export FZF_ALT_C_OPTS=$FZF_OPTS
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,28 +1,68 @@
 set -g fish_greeting
-set -x EDITOR micro
-# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 if status is-interactive
-    alias ll='eza -lah'
     alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-    # starship config
-    set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
-    starship init fish | source
+    alias clc='clear'
+    if command -q kubectl
+        alias kube='kubectl'
+    end
+    if command -q flatpak
+        alias zed='flatpak run dev.zed.Zed'
+        alias edge='flatpak run com.microsoft.Edge'
+        alias zen='flatpak run io.github.zen_browser.zen'
+    end
+    # editor
+    if command -q micro
+        set -x EDITOR (which micro)
+        set -x VISUAL (which micro)
+    end
+    # better ls (eza)
+    if command -q eza
+        alias ll='eza -lah'
+    end
+    # colors
+    if command -q vivid
+        set -Ux LS_COLORS $(vivid generate catppuccin-mocha)
+    end
+    # prompt
+    if command -q starship
+        set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
+        starship init fish | source
+    end
     # micro
-    set -x MICRO_TRUECOLOR 1
-    set -x TERM xterm-256color
+    if command -q micro
+        set -x MICRO_TRUECOLOR 1
+        set -x TERM xterm-256color
+    end
+
     # fzf
-    set -x FZF_DEFAULT_OPTS " \
-    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-    --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a \
-    --multi --tmux 80% --layout default
-    "
-    set -x fzf_preview_dir_cmd eza  --color=always -laah
-    set -x fzf_preview_file_cmd bat --color=always -n
-    set -x fzf_diff_highlighter delta --paging=never --width=20
-    set -x fzf_fd_opts --ignore-case --hidden --exclude .git --max-depth 6 --color=always
-    set -x fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
-    fzf_configure_bindings --directory=\cf --variables=\e\cv
+    if command -q fzf
+        # fzf --fish | source
+        set -x FZF_DEFAULT_OPTS " \
+        --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+        --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+        --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a \
+        --multi --tmux 80% --layout default
+        "
+        set -x fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+        fzf_configure_bindings --directory=\cf --variables=\e\cv
+        if command -q fd
+            set -x fzf_fd_opts --ignore-case --hidden --exclude .git --max-depth 6 --color=always
+        end
+        if command -q eza
+            set -x fzf_preview_dir_cmd eza  --color=always --icons=always -laah
+        end
+        if command -q bat
+            set -x fzf_preview_file_cmd bat --color=always -n
+        end
+        if command -q delta
+            set -x fzf_diff_highlighter delta --paging=never --width=20
+        end
+    end
     # zoxide
-    zoxide init fish | source
+    if command -q zoxide
+        zoxide init fish | source
+    end
+    # task
+    set -x TASKRC ~/.config/task/config
+    # set -x TASKDATA ~/.local/task
 end
