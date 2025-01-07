a7fbadc0aab2b8fdceff9c9ca314b120d0fdb377
forest reider <forest.reider@belimo.ch>
2025-01-07 11:17:20 +0100

    reduce number of curl calls

--- dev-setup/setup-shell.sh
+++ dev-setup/setup-shell.sh
@@ -1,8 +1,15 @@
 #! /usr/bin/bash
 
-# target directory
-TARGET="$HOME/.local/bin"
-# TARGET="$HOME/dev-setup/test_bin"
+## setup
+
+# directories
+TEMP_DIR=$(mktemp -d)
+TEMP_DIR="$HOME/dev-setup/test_bin"
+FISH_COMPLETIONS_DIR="$HOME/.config/fish/completions"
+LOCAL_BIN_DIR="$HOME/.local/bin"
+
+# create directories
+mkdir -p $FISH_COMPLETIONS_DIR
 
 # get the latest release info from a github repo
 get_latest_release_info() {
@@ -20,73 +27,156 @@ get_download_url() {
     echo "https://github.com/$owner/$repo/releases/download/$latest/$file"
 }
 
+release_urls() {
+    local url=$1
+    local file=$2
+    echo ""
+    echo $url
+    echo $url >> $file
+}
 
-# # fish
-# owner="fish-shell"
-# repo="fish-shell"
-# latest=$(get_latest_release_info $owner $repo)
-# echo $latest
-# latest="4.0b1"
-# file="fish-static-linux-x86_64.tar.xz"
-# url=$(get_download_url $owner $repo $latest $file)
-# echo $url
-# curl -L $url | tar -xvJf - -C $TARGET
+trim_file() {
+    local file=$1
+    echo "${file%%.tar.*}"
+}
+
+# create a versions file year-month-date prefix
+script_dir=$(dirname "$(readlink -f "$0")")
+releases="$(dirname "$(readlink -f "$0")")/versions_$(date +%Y-%m-%d).txt"
+if [ -f "$releases" ]; then
+    rm -f "$releases"
+fi
+touch -f $releases
+
+## install binaries
+
+# bat
+owner="sharkdp"
+repo="bat"
+latest=$(get_latest_release_info $owner $repo)
+file="bat-$latest-x86_64-unknown-linux-musl.tar.gz"
+folder=$(trim_file $file)
+url=$(get_download_url $owner $repo $latest $file)
+release_urls $url $releases
+curl -L $url | tar -xzf - -C $TEMP_DIR
+cp -f $TEMP_DIR/$folder/bat $LOCAL_BIN_DIR
+cp -f $TEMP_DIR/$folder/autocomplete/bat.fish $FISH_COMPLETIONS_DIR
+
+# eza
+owner="eza-community"
+repo="eza"
+latest=$(get_latest_release_info $owner $repo)
+file="eza_x86_64-unknown-linux-musl.tar.gz"
+folder=$(trim_file $file)
+url=$(get_download_url $owner $repo $latest $file)
+release_urls $url $releases
+curl -L $url | tar -xzf - -C $TEMP_DIR
+cp -f $TEMP_DIR/eza $LOCAL_BIN_DIR
+
+# fd
+owner="sharkdp"
+repo="fd"
+latest=$(get_latest_release_info $owner $repo)
+file="fd-$latest-x86_64-unknown-linux-musl.tar.gz"
+folder=$(trim_file $file)
+url=$(get_download_url $owner $repo $latest $file)
+release_urls $url $releases
+curl -L $url | tar -xzf - -C $TEMP_DIR
+cp -f $TEMP_DIR/$folder/fd $LOCAL_BIN_DIR
+cp -f $TEMP_DIR/$folder/autocomplete/fd.fish $FISH_COMPLETIONS_DIR
+
+# fish
+owner="fish-shell"
+repo="fish-shell"
+latest=$(get_latest_release_info $owner $repo)
+latest="4.0b1"
+file="fish-static-linux-x86_64.tar.xz"
+folder=$(trim_file $file)
+url=$(get_download_url $owner $repo $latest $file)
+release_urls $url $releases
+curl -L $url | tar -xJf - -C $TEMP_DIR
+cp -f $TEMP_DIR/fish* $LOCAL_BIN_DIR
+
+# numbat
+owner="sharkdp"
+repo="numbat"
+latest=$(get_latest_release_info $owner $repo)
+file="numbat-$latest-x86_64-unknown-linux-musl.tar.gz"
+folder=$(trim_file $file)
+url=$(get_download_url $owner $repo $latest $file)
+release_urls $url $releases
+curl -L $url | tar -xzf - -C $TEMP_DIR # --strip-components=1 --wildcards '**/*numbat'
+cp -f $TEMP_DIR/$folder/numbat $LOCAL_BIN_DIR
+cp -rf $TEMP_DIR/$folder/modules $HOME/.config/numbat
+
+# nushell
+owner="nushell"
+repo="nushell"
+latest=$(get_latest_release_info $owner $repo)
+file="nu-$latest-x86_64-unknown-linux-musl.tar.gz"
+folder=$(trim_file $file)
+url=$(get_download_url $owner $repo $latest $file)
+release_urls $url $releases
+curl -L $url | tar -xzf - -C $TEMP_DIR
+cp -f $TEMP_DIR/$folder/nu* $LOCAL_BIN_DIR
+
+# micro
+owner="zyedidia"
+repo="micro"
+latest=$(get_latest_release_info $owner $repo)
+file="micro-$latest-linux64-static.tar.gz"
+folder=$(echo "micro-$latest" | sed 's/micro-v/micro-/')
+url=$(get_download_url $owner $repo $latest $file)
+url=$(echo $url | sed 's/micro-v/micro-/')
+release_urls $url $releases
+curl -L $url | tar -xzf - -C $TEMP_DIR
+cp -f $TEMP_DIR/$folder/micro $LOCAL_BIN_DIR
+
+# ripgrep
+owner="BurntSushi"
+repo="ripgrep"
+latest=$(get_latest_release_info $owner $repo)
+file="ripgrep-$latest-x86_64-unknown-linux-musl.tar.gz"
+folder=$(trim_file $file)
+url=$(get_download_url $owner $repo $latest $file)
+release_urls $url $releases
+curl -L $url | tar -xzf - -C $TEMP_DIR
+cp -f $TEMP_DIR/$folder/rg $LOCAL_BIN_DIR
+cp -f $TEMP_DIR/$folder/complete/rg.fish $FISH_COMPLETIONS_DIR
+
+# starship
+owner="starship"
+repo="starship"
+latest=$(get_latest_release_info $owner $repo)
+file="starship-x86_64-unknown-linux-musl.tar.gz"
+folder=$(trim_file $file)
+url=$(get_download_url $owner $repo $latest $file)
+release_urls $url $releases
+curl -L $url | tar -xzf - -C $TEMP_DIR
+cp -f $TEMP_DIR/starship $LOCAL_BIN_DIR
+
+# vivid
+owner="sharkdp"
+repo="vivid"
+latest=$(get_latest_release_info $owner $repo)
+file="vivid-$latest-x86_64-unknown-linux-musl.tar.gz"
+folder=$(trim_file $file)
+url=$(get_download_url $owner $repo $latest $file)
+release_urls $url $releases
+curl -L $url | tar -xzf - -C $TEMP_DIR
+cp -f $TEMP_DIR/$folder/vivid $LOCAL_BIN_DIR
+
+# zoxide
+owner="ajeetdsouza"
+repo="zoxide"
+latest=$(get_latest_release_info $owner $repo)
+file="zoxide-$latest-x86_64-unknown-linux-musl.tar.gz"
+folder=$(trim_file $file)
+url=$(get_download_url $owner $repo $latest $file | sed 's/zoxide-v/zoxide-/')
+release_urls $url $releases
+curl -L $url | tar -xzf - -C $TEMP_DIR
+cp -f $TEMP_DIR/zoxide $LOCAL_BIN_DIR
 
 
-# # nushell
-# owner="nushell"
-# repo="nushell"
-# latest=$(get_latest_release_info $owner $repo)
-# file="nu-$latest-x86_64-unknown-linux-musl.tar.gz"
-# url=$(get_download_url $owner $repo $latest $file)
-# curl -L $url | tar -xvzf - -C $TARGET --strip-components=1 --wildcards '**/nu*'
-
-
-# # starship
-# owner="starship"
-# repo="starship"
-# latest=$(get_latest_release_info $owner $repo)
-# file="starship-x86_64-unknown-linux-musl.tar.gz"
-# url=$(get_download_url $owner $repo $latest $file)
-# curl -L $url | tar -xvzf - -C $TARGET
-
-
-# # vivid
-# owner="sharkdp"
-# repo="vivid"
-# latest=$(get_latest_release_info $owner $repo)
-# file="vivid-$latest-x86_64-unknown-linux-musl.tar.gz"
-# url=$(get_download_url $owner $repo $latest $file)
-# curl -L $url | tar -xvzf - -C $TARGET --strip-components=1 --wildcards '**/vivid'
-
-
-# # numbat
-# owner="sharkdp"
-# repo="numbat"
-# latest=$(get_latest_release_info $owner $repo)
-# file="numbat-$latest-x86_64-unknown-linux-musl.tar.gz"
-# url=$(get_download_url $owner $repo $latest $file)
-# curl -L $url | tar -xvzf - -C $TARGET --strip-components=1 --wildcards '**/*numbat'
-# # include modules in the .config/numbat directory
-# mkdir -p $HOME/.config/numbat
-# curl -L $url | tar -xvzf - -C $HOME/.config/numbat --strip-components=1 --wildcards '**/*.nbt'
-
-
-# # micro
-# owner="zyedidia"
-# repo="micro"
-# latest=$(get_latest_release_info $owner $repo)
-# file="micro-$latest-linux64-static.tar.gz"
-# url=$(get_download_url $owner $repo $latest $file)
-# url=$(echo $url | sed 's/micro-v/micro-/')
-# curl -L $url | tar -xvzf - -C $TARGET --strip-components=1 --wildcards '**/micro'
-
-
-# # eza
-# owner="eza-community"
-# repo="eza"
-# latest=$(get_latest_release_info $owner $repo)
-# file="eza_x86_64-unknown-linux-musl.tar.gz"
-# url=$(get_download_url $owner $repo $latest $file)
-# url=$(echo $url | sed 's/micro-v/micro-/')
-# curl -L $url | tar -xvzf - -C $TARGET --strip-components=1
+## cleanup
+# rm -f $TEMP_DIR
